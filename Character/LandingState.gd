6883c56baf599f947614a6796febf14256e60bb6
coatlessali <coatlessali@protonmail.com>
2023-04-29 06:09:32 -0500

    change how landing works to be instant
    also added a non-functioning dust cloud that is never called

--- Character/LandingState.gd
+++ Character/LandingState.gd
@@ -6,13 +6,13 @@ class_name LandingState
 @export var braking_state : State
 @export var running_state : State
 
-func _on_animation_tree_animation_finished(anim_name):
-	if(anim_name == "jump_end"):
+func _physics_process(delta):
+#	if(anim_name == "jump_end"):
 		# see if we need to go into running or braking
-		if character.local_velocity.x != 0:
-			if character.input_direction.x != 0: 
-				next_state = running_state
-			else:
-				next_state = braking_state
+	if character.local_velocity.x != 0:
+		if character.input_direction.x != 0: 
+			next_state = running_state
 		else:
-			next_state = ground_state
+			next_state = braking_state
+	else:
+		next_state = ground_state
